81f0ef4cff80b150fc165a2973d52f9a5b2f4cf4
berkleylim <berkleylim16@gmail.com>
2023-11-13 22:11:04 +0900

    feat: presentation&container component 리마인드 - customHooks으로 변경하기 위한 컴포넌트 전 예제로 추가 - 현재 ver1 그대로 사용

---
+++ react_cs/src/ver2/presentation_container/Container.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react'
+import Presentation from './Presentation'
+
+// 인터페이스 설정 - 이름
+type NameInterface = {
+  name: string
+}
+
+// 인터페이스 설정 - 나이
+type AgeInterface = {
+  name: string
+}
+
+
+// Container 컴포넌트 - 로직 처리
+const Container = () => {
+  const [name, setName] = useState<NameInterface>();
+  const [age, setAge] = useState<AgeInterface>();
+
+  const onNameChange = (e:any) => {
+    const {name, value} = e.currentTarget;
+    setName({
+      [name] :value
+    } as NameInterface)
+  }
+  
+  const onAgeChange = (e:any) => {
+    const {name, value} = e.currentTarget
+    setAge({
+      [name]: value
+    } as AgeInterface)
+  }
+
+  // 테스트용
+  useEffect(() => {
+    console.log(name)
+    console.log(age)
+  }, [name, age])
+
+  return (
+    <Presentation
+      onNameChange={onNameChange}
+      onAgeChange={onAgeChange}
+    />
+  )
+}
+
+export default Container
